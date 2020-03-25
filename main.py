166433e54a4667475486cb0c11a317e486d60d72
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-03-25 11:29:50 +0530

    Case Printing

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ from get_source import connect_source
 source = connect_source()
 
 
+soup = BeautifulSoup(source.text, 'html.parser')
 
+tf = soup.findAll('td')
+
+for i in range(len(tf)):
+    if tf[i].text == "India":
+        print("India")
+        print(f"Total Cases:{tf[i+1].text} ")
+        print(f"New Cases: {tf[i+2].text}")
+        print(f"Total Death: {tf[i+3].text}")
+        print(f"New Death:{tf[i+4].text} ")
+        break
+    
+print("............................")
+
+active_case = soup.find("div", attrs = {'class':"number-table-main"})
+print("International Active Cases: ",active_case.text)
 
