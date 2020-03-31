6f644ce678400e22637b2c0b046fac700ce3d2eb
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-03-31 10:40:01 +0530

    html rendered

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,8 +1,8 @@
-from flask import Flask 
+from flask import Flask , render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Home</h1>"
+    return render_template("index.html")
 
